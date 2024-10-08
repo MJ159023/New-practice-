b8c56445ba10d9b0d6314d35ed1fdacad5209e27
MJ159023 <michael.bruneau@stu.ocsb.ca>
2024-10-08 16:49:51 +0000

    added comments

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ a = 5
 b = 3
 
 def hypotenuse(sides_1, side_2):
+    """Caculates teh hypotenuse of a right angle triangle"""
     hypotenuse_length = math.sqrt(a**2 + b**2)
     return hypotenuse_length
 
